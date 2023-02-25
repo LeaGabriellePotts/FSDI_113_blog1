40a69194b6c973e98d7e82d2c37f0fec024be27d
Lea P <lea.potts.ord@gmail.com>
2023-02-25 13:24:28 -0500

    class2 first commit

--- posts/urls.py
+++ posts/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from .views import (
     PostListView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    PostDetailView,
+    PostCreateView
 )
 
 urlpatterns = [
     path("", PostListView.as_view(), name="list"),
+    path("new/", PostCreateView.as_view(), name="new"),
+    path("<int:pk>/", PostDetailView.as_view(), name="detail"),
     path("<int:pk>/edit/", PostUpdateView.as_view(), name="edit"),
     path("<int:pk>/delete/", PostDeleteView.as_view(), name="delete"),
 ]
